ba6f513a7f035ebcb0bf0af0716b2475acd38ea4
Collin Worth <worthcollin@gmail.com>
2025-06-22 17:44:17 -0700

    latest working

---
+++ gitcookin/GetCookin/app/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to GitCookin!</h1>
+      <p>Your one-stop destination for delicious recipes and cooking inspiration.</p>
+    </div>
+  );
+}
+
+export default Home;
